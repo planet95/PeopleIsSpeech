82e388763b887cccc585a880f103e3f5ea178c2c
planet95 <planet95@gmail.com>
2014-05-22 21:24:53 -0700

    bootstrap responsiveness!

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,9 @@
 ﻿var request = require('request');
 exports.legislators = function(req, res){
     var url = 'https://congress.api.sunlightfoundation.com/legislators/locate?latitude='+req.query.lat+'&longitude='+req.query.lon+'&apikey=4f40f44747c44a22ba19287b9e953e4c';
-    request(url, function (error, response, body) {
+    request(url, function (error, response, data) {
   if (!error && response.statusCode == 200) {
-     var data = JSON.parse(body);
+     var data = JSON.parse(data);
     res.render('data',{data:data.results});
   // res.render('data', { body: 'PplIsSpeech'});
    }
